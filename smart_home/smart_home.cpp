13147c4a3b6ab5507941057e836b069613aa43af
Aleksander <nsk540010@gmail.com>
2023-11-07 23:12:23 +0700

    Отправил на проверку готовую версию.

--- smart_home/smart_home.cpp
+++ smart_home/smart_home.cpp
@@ -12,74 +12,13 @@ WATER_PIPE_HEATING = 8,
 CONDITIONER = 16
 };
 
-int  water_pipe(int temperature_outside,int switches_state)      // включение - выключение ОБОГРЕВА ПОМПЫ
-{
-    if (temperature_outside < 0)                            // если темпер < 0 включаем обогрев помпы 
-    {
-        switches_state |= WATER_PIPE_HEATING;
-    }
-    if (temperature_outside > 5)
-    {
-        switches_state &= ~WATER_PIPE_HEATING;              // если темпер  > 5 отключаем обогрев помпы
-    }
-    return switches_state;
-}
-int  weather_in_the_house(int temperature_inside, int switches_state)      // включение - выключение ОТОПЛЕНИЯ И КОНДИЦИОНЕРА
-{
-    if (temperature_inside < 22)                            // если темпер < 22 включаем ОТОПЛЕНИЕ
-    {
-        switches_state |= HEATERS;
-    }
-    if (temperature_inside > 25 )
-    {
-        switches_state &= ~HEATERS;              // если темпер  > 25 отключаем ОТОПЛЕНИЕ
-    }
-    if (temperature_inside > 30)
-    {
-        switches_state |= CONDITIONER;           // если темпер >  30 включаем кондер
-    }
-    if (temperature_inside < 26)                 // если темпер < 25 и ниже  отключаем кондер
-    {
-        switches_state &= ~CONDITIONER;
-    }
-    return switches_state;
-}
-int garden_lighting(int i,bool movement,int switches_state)
-{
-    if (i < 5 && movement || i > 16 && i < 29 && movement || i > 40 && i < 48 && movement)   // если вечером есть движение - включаем свет
-    {
-        switches_state |= LIGHTS_OUTSIDE;
-    }
-    if (i < 5 && !movement || i > 16 && i < 29 && !movement || i > 40 && i < 48 && !movement)   // если вечером нет движения - ВЫключаем свет
-    {
-        switches_state &= ~LIGHTS_OUTSIDE;
-    }
-    if (i > 4 && i < 17 || i > 28 && i < 41)                                                   // в дневное время отключаем свет в саду
-    {
-        switches_state &= ~LIGHTS_OUTSIDE;
-    }
-    return switches_state;
-}
-void light_in_the_house(int i, bool lights, int switches_state)                               // НУЖНО ДОПИСАТЬ СВЕТ В ДОМЕ
-{
-    if (lights)
-    {
-
-    }
-}
-
-
-bool zero_bit(int switches_state, int index)            // проверка включен ли бит
-{
-    return (switches_state & (1 << index)) != 0;
-}
 
 int main()
 {
     
     int switches_state = 0;                            // переменная, хранящая информацию о работающих датчиках
-   // switches_state |= HEATERS;                       // чтобы включить обогреватель
-    // switches_state &= ~HEATERS;                     // чтобы выключить обогреватель
+
+    int color_temperature = 5000;
     int temperature_inside = 0;
     int temperature_outside = 0;
     bool movement = "yes";
@@ -95,30 +34,81 @@ int main()
         std::stringstream temp_stream(teams);           // создаем строковый поток
         temp_stream >> temperature_inside >> temperature_outside >> movement_word >> lights_word;
 
-        
+
         movement = (movement_word == "yes") ? true : false;     // переводим в булевское значение строку да(1) или нет(0)
         lights = (lights_word == "on") ? true : false;          // переводим в булевское значение строку да(1) или нет(0)
 
-        switches_state = water_pipe(temperature_outside, switches_state);
-        switches_state = weather_in_the_house(temperature_inside, switches_state);
-        switches_state = garden_lighting(i, movement, switches_state);
+        if (temperature_outside < 0 && !(switches_state & WATER_PIPE_HEATING))     //  если темпер снаружи  < 0 и помпа выключена
+        {
+            switches_state |= WATER_PIPE_HEATING;                                  // включаем помпу
+            std::cout << "Water pump heating ON!\n";
+        }
+        if (temperature_outside > 5 && (switches_state & WATER_PIPE_HEATING))      // если темпер  > 5  и помпа включена
+        {
+            switches_state &= ~WATER_PIPE_HEATING;                                 //  отключаем обогрев помпы
+            std::cout << "Water pump heating OFF!\n";
+        }
 
-        (zero_bit(switches_state, HEATERS)) ? std::cout << "Heaters ON!\n" : std::cout << "Heaters OFF!\n";
+        if (temperature_inside < 22 && !(switches_state & HEATERS))                            // если темпер < 22 и отопление выключено
+        {
+            switches_state |= HEATERS;                                                        // включаем ОТОПЛЕНИЕ
+            std::cout << "Heaters ON!\n";
+        }
+        if (temperature_inside > 25 && (switches_state & HEATERS))                            // если темпер  > 25 и отопление включено
+        {
+            switches_state &= ~HEATERS;                                                       //  отключаем ОТОПЛЕНИЕ
+            std::cout << "Heaters OFF!\n";
+        }
 
-        (zero_bit(switches_state, CONDITIONER)) ? std::cout << "Conditioner ON!\n" : std::cout << "Conditioner OFF!\n";
+        if (temperature_inside > 29 && !(switches_state & CONDITIONER))                       // если темпер >  30 и кондиционер выключен
+        {
+            switches_state |= CONDITIONER;                                                    //   30 включаем кондер
+            std::cout << "Conditioner ON!!\n";
+        }
+        if (temperature_inside < 25 && (switches_state & CONDITIONER))                 // если темпер < 25 и ниже и кондиционер включен отключаем кондер
+        {
+            switches_state &= ~CONDITIONER;                                            // отключаем кондер
+            std::cout << "Conditioner OFF!!\n";
+        }
 
-        (zero_bit(switches_state, LIGHTS_OUTSIDE)) ? std::cout << "Lights ON!\n" : std::cout << "Lights OFF!\n";
-        
-        (zero_bit(switches_state, WATER_PIPE_HEATING)) ? std::cout << "heating water supply ON!\n" : std::cout << "heating water supply OFF!\n";
+        if (i < 5 && movement && !(switches_state & LIGHTS_OUTSIDE) || i > 16 && i < 29 && movement && !(switches_state & LIGHTS_OUTSIDE) || i > 40 && i < 48 && movement && !(switches_state & LIGHTS_OUTSIDE))   // если вечером есть движение и выключен свет 
+        {
+            switches_state |= LIGHTS_OUTSIDE;                                          // Включаем свет
+            std::cout << "Lights ON!\n";
+        }
+        if (i < 5 && !movement && (switches_state & LIGHTS_OUTSIDE) || i > 16 && i < 29 && !movement && (switches_state & LIGHTS_OUTSIDE) || i > 40 && i < 48 && !movement && (switches_state & LIGHTS_OUTSIDE))   // если вечером нет движения и включен свет
+        {
+            switches_state &= ~LIGHTS_OUTSIDE;                                         // ВЫКЛЮЧАЕМ СВЕТ
+            std::cout << "Lights OFF!\n";
+        }
+        if (i > 4 && i < 17 && (switches_state & LIGHTS_OUTSIDE) || i > 28 && i < 41 && (switches_state & LIGHTS_OUTSIDE))                                                   // в дневное время отключаем свет в саду,  если он был включен
+        {
+            switches_state &= ~LIGHTS_OUTSIDE;
+            std::cout << "Lights OFF!\n";
+        }
+        // циклы для цветовой температуры внутри дома
+       if (i >= 16 && i < 20)
+        {
+            color_temperature -= 575;
+        }
+        if (i == 24)
+        {
+            color_temperature = 5000;
+        }
+        if (i >= 40 && i < 44)
+        {
+            color_temperature -= 575;
+        }
+        if (lights)
+        {
+            std::cout << "Color temperature :" << color_temperature << "K\n";
+        }
 
-
-
-        //std::cout << temperature_inside;
-       // std::cout << temperature_outside;
-        //std::cout << movement;
-        //std::cout << lights;
     }
+       
 
+        
+    return 0;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
